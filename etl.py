555db5353e8fc90dfe329ccebb794e4de7eed684
Joao Junior <joao.junior@catenamedia.com>
2019-01-17 18:03:25 +0100

    Add the parser

--- etl.py
+++ etl.py
@@ -5,12 +5,25 @@ from constants import START_YEAR, FINISH_YEAR, URL
 
 def crawler(url):
     r = requests.get(url)
-    print(url, r.status_code)
+    return r.json()
+
+
+def parser(population_data):
+    result = {}
+    women = 0
+    men = 0
+    for row in population_data:
+        women += row['females']
+        men += row['males']
+    return women, men
 
 
 def get_all_data():
     for year in range(START_YEAR, FINISH_YEAR + 1):
-        crawler(URL.format(year))
+        result = crawler(URL.format(year))
+        women, men = parser(result)
+        print(year,' Rate = ', women / men)
+
 
 
 if __name__ == '__main__':
