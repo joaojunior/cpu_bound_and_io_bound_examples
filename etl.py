fe85934e540a98374f9d49e30d037c5e7bf83dd7
Joao Junior <joao.junior@catenamedia.com>
2019-01-17 17:23:20 +0100

    Add thread in the crawler

--- etl.py
+++ etl.py
@@ -1,3 +1,5 @@
+from threading import Thread
+
 import requests
 
 from constants import START_YEAR, FINISH_YEAR, URL
@@ -9,8 +11,16 @@ def crawler(url):
 
 
 def get_all_data():
+    threads = []
+
     for year in range(START_YEAR, FINISH_YEAR + 1):
-        crawler(URL.format(year))
+        threads.append(Thread(target=crawler, args=(URL.format(year), )))
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
 
 
 if __name__ == '__main__':
