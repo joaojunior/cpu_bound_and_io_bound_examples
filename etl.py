d6c75534152482c8abe7fabbb2b46ace5eb7d0cd
Joao Junior <joao.junior@catenamedia.com>
2019-01-17 19:08:59 +0100

    Add asyncio

--- etl.py
+++ etl.py
@@ -1,28 +1,40 @@
 from typing import List, Dict, Tuple
 
-import requests
+import asyncio
+import aiohttp
 
 from constants import START_YEAR, FINISH_YEAR, URL
 
 
-def crawler(url: str) -> List[Dict]:
-    r = requests.get(url)
-    return r.json()
+async def crawler(url: str) -> List[Dict]:
+    async with aiohttp.ClientSession() as session:
+        response = await session.get(url)
+        return await response.json()
 
 
-def parser(population_data: Dict) -> Tuple[int, int]:
+def parser(population_data: Dict) -> Tuple[int, int, int]:
+    year = 0
     women = 0
     men = 0
     for row in population_data:
+        year = row['year']
         women += row['females']
         men += row['males']
-    return women, men
+    return women, men, year
 
 
 def get_all_data():
+    loop = asyncio.get_event_loop()
+    tasks = []
+
     for year in range(START_YEAR, FINISH_YEAR + 1):
-        result = crawler(URL.format(year))
-        women, men = parser(result)
+        tasks.append(
+            asyncio.ensure_future(crawler(URL.format(year)))
+        )
+
+    loop.run_until_complete(asyncio.gather(*tasks))
+    for task in tasks:
+        women, men, year = parser(task.result())
         print(year, ' Rate = ', women / men)
 
 
